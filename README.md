5e621b5478c1cbcf131e564b8c77609f3de41db7
jfaria23 <127213868+jfaria23@users.noreply.github.com>
2023-05-10 09:37:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StackBlitz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-mtg3su)
---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('hello world');
+
+let amount = 10;
+
+console.log(amount);
+
+// const === fica fixo ==== global/ block scope
+// let === permite alterar ==== global/ block scope
